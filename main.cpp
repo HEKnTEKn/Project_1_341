c5911aa625bfa99ceda3b089fe909de139499f3a
Victor Fong <djtekno11@gmail.com>
2018-09-11 21:38:07 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,56 @@
+/*main.cpp*/
+
+//
+// Grade Analysis program in modern C++.
+//
+// << Victor Fong >>
+// U. of Illinois, Chicago
+// CS 341: Fall 2018
+// Project 01
+//
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+class Course
+{
+private:
+
+public:
+
+};
+
+.0
+
+
+int main()
+{
+    string filename;
+
+    cin >> filename;
+    cout << filename << endl;
+    cout << endl;
+
+    cout << std::fixed;
+    cout << std::setprecision(2);
+
+
+
+
+
+
+
+    //
+    // done:
+    //
+    cout << endl;
+    return 0;
+}
